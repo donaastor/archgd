cee5647eaaa1c06ea1d66686d6a8271ea598c133
donaastor <45340801+donaastor@users.noreply.github.com>
2022-04-02 02:56:54 +0200

    Update ins-chroot.sh

--- scripts/ins-chroot.sh
+++ scripts/ins-chroot.sh
@@ -115,14 +115,14 @@ reconnect() {
 
 #			user
 
-echo "Press enter [passwd root]"; read line
+# echo "Press enter [passwd root]"; read line
 echo "For root!"
 while ! passwd; do
   echo "Please try again"
 done
-echo "Press enter [useradd]"; read line
+# echo "Press enter [useradd]"; read line
 useradd -mg wheel $username
-echo "Press enter [passwd user]"; read line
+# echo "Press enter [passwd user]"; read line
 echo "For user!"
 while ! passwd $username; do
   echo "Please try again"
@@ -130,177 +130,177 @@ done
 
 #			pacman
 
-echo "Press enter [sed pacman]"; read line
+# echo "Press enter [sed pacman]"; read line
 sed -i 's/^#Color/Color/' /etc/pacman.conf
 
 #			jezik i vreme
 
-echo "Press enter [cd /etc]"; read line
+# echo "Press enter [cd /etc]"; read line
 cd /etc
-echo "Press enter [sed locale.gen]"; read line
+# echo "Press enter [sed locale.gen]"; read line
 sed -i 's/#en_US/en_US/g' locale.gen
-echo "Press enter [locale-gen]"; read line
+# echo "Press enter [locale-gen]"; read line
 locale-gen
-echo "Press enter [locale.conf]"; read line
+# echo "Press enter [locale.conf]"; read line
 echo "LANG=en_US.UTF-8" >> locale.conf
-echo "Press enter [hostname]"; read line
+# echo "Press enter [hostname]"; read line
 echo arch >> hostname
-echo "Press enter [localtime]"; read line
+# echo "Press enter [localtime]"; read line
 ln -sf /usr/share/zoneinfo/Europe/Belgrade /etc/localtime
 
 #			more tools
 
-echo "Press enter [pacman more tools]"; read line
+# echo "Press enter [pacman more tools]"; read line
 while ! pacman -S --noconfirm --needed networkmanager grub git base-devel; do
   reconnect
 done
 
 #			sudo
 
-echo "Press enter [sed sudoers]"; read line
+# echo "Press enter [sed sudoers]"; read line
 sed -i 's/^# %sudo/%sudo/' /etc/sudoers
-echo "Press enter [groupadd sudo]"; read line
+# echo "Press enter [groupadd sudo]"; read line
 groupadd sudo
-echo "Press enter [gpasswd]"; read line
+# echo "Press enter [gpasswd]"; read line
 gpasswd -a $username sudo
 
 #			internet
 
 if [ $WIFI = 0 ]; then
-  echo "Press enter [network manager]"; read line
+#   echo "Press enter [network manager]"; read line
   systemctl enable NetworkManager
 else
-  echo "Press enter [pacman iwd]"; read line
+#   echo "Press enter [pacman iwd]"; read line
   while ! pacman -S --noconfirm --needed iwd; do
     reconnect
   done
-  echo "Press enter [mkdir /etc/iwd]"; read line
+#   echo "Press enter [mkdir /etc/iwd]"; read line
   mkdir /etc/iwd
-  echo "Press enter [wifi dns]"; read line
+#   echo "Press enter [wifi dns]"; read line
   printf "\n\n[General]\nEnableNetworkConfiguration=true\n" > /etc/iwd/main.conf
-  echo "Press enter [wifi resolv]"; read line
+#   echo "Press enter [wifi resolv]"; read line
   printf "\n\nnameserver 8.8.8.8" >> /etc/resolv.conf
-  echo "Press enter [network manager, iwd]"; read line
+#   echo "Press enter [network manager, iwd]"; read line
   systemctl enable NetworkManager iwd
 fi
 
 #			fstab
 
-echo "Press enter [fstab_radni]"; read line
+# echo "Press enter [fstab_radni]"; read line
 printf "\ntmpfs /root/tren tmpfs defaults,size=2048M 0 0\ntmpfs /home/$username/tren tmpfs defaults,size=2048M 0 0\ntmpfs /home/$username/.cache/pikaur tmpfs default 0 0\ntmpfs /home/$username/.local/share/pikaur/aur_repos tmpfs defaults,size=2048M 0 0\ntmpfs /var/lib/systemd/coredumps tmpfs defaults,size=512M 0 0\ntmpfs /home/$username/.cargo tmpfs defaults,size=640M 0 0\ntmpfs /home/$username/chromium/cache tmpfs noatime,nodev,nosuid,size=1152M 0 0" >> "/root/tren/fstab_radni"
-echo "Press enter [fstab_radni > fstab]"; read line
+# echo "Press enter [fstab_radni > fstab]"; read line
 cp /root/tren/fstab_radni /etc/fstab
-echo "Press enter [cd home]"; read line
+# echo "Press enter [cd home]"; read line
 cd "/home/$username"
-echo "Press enter [mkdir .cache]"; read line
+# echo "Press enter [mkdir .cache]"; read line
 sudo -u "$username" mkdir .cache
-echo "Press enter [mkdir .cache/pikaur]"; read line
+# echo "Press enter [mkdir .cache/pikaur]"; read line
 sudo -u "$username" mkdir .cache/pikaur
-echo "Press enter [mkdir .local]"; read line
+# echo "Press enter [mkdir .local]"; read line
 sudo -u "$username" mkdir .local
-echo "Press enter [mkdir .local/share]"; read line
+# echo "Press enter [mkdir .local/share]"; read line
 sudo -u "$username" mkdir .local/share
-echo "Press enter [mkdir .local/share/pikaur]"; read line
+# echo "Press enter [mkdir .local/share/pikaur]"; read line
 sudo -u "$username" mkdir .local/share/pikaur
-echo "Press enter [mkdir .local/share/pikaur/aur_repos]"; read line
+# echo "Press enter [mkdir .local/share/pikaur/aur_repos]"; read line
 sudo -u "$username" mkdir .local/share/pikaur/aur_repos
-echo "Press enter [mkdir /var/lib/systemd/coredumps]"; read line
+# echo "Press enter [mkdir /var/lib/systemd/coredumps]"; read line
 mkdir /var/lib/systemd/coredumps
-echo "Press enter [mkdir .cargo]"; read line
+# echo "Press enter [mkdir .cargo]"; read line
 sudo -u "$username" mkdir .cargo
 
 #			grub
 
 if [ $EFI = 1 ]; then
-  echo "Press enter [pacman efibootmgr]"; read line
+#   echo "Press enter [pacman efibootmgr]"; read line
   while ! pacman -S --noconfirm --needed efibootmgr; do
     reconnect
   done
-  echo "Press enter [grub-install]"; read line
+#   echo "Press enter [grub-install]"; read line
   grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 else
-  echo "Press enter [grub-install]"; read line
+#   echo "Press enter [grub-install]"; read line
   grub-install $drive_name
 fi
-echo "Press enter [mkdir /tmp/grub_radni]"; read line
+# echo "Press enter [mkdir /tmp/grub_radni]"; read line
 mkdir /tmp/grub_radni
-echo "Press enter [cd /tmp/grub_radni]"; read line
+# echo "Press enter [cd /tmp/grub_radni]"; read line
 cd /tmp/grub_radni
-echo "Press enter [cp /etc/default/grub grub]"; read line
+# echo "Press enter [cp /etc/default/grub grub]"; read line
 cp /etc/default/grub grub
-echo "Press enter [sed GRUB_TIMEOUT]"; read line
+# echo "Press enter [sed GRUB_TIMEOUT]"; read line
 sed -i 's/#GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/' grub
 if [ $AMD_GPU = 1 ]; then
-  echo "Press enter [sed KERNEL, GPU]"; read line
+#   echo "Press enter [sed KERNEL, GPU]"; read line
   sed -i 's/\(^GRUB_CMDLINE_LINUX_DEFAULT.*\)\"/\1 amdgpu.ppfeaturemask=0xffffffff mitigations=off\"/' grub
 else
-  echo "Press enter [sed KERNEL]"; read line
+#   echo "Press enter [sed KERNEL]"; read line
   sed -i 's/\(^GRUB_CMDLINE_LINUX_DEFAULT.*\)\"/\1 mitigations=off\"/' grub
 fi
 if [ $AMD_CPU = 0 ]; then
-  echo "Press enter [pacman intel-ucode]"; read line
+#   echo "Press enter [pacman intel-ucode]"; read line
   while ! pacman -S --noconfirm --needed intel-ucode; do
     reconnect
   done
 else
-  echo "Press enter [pacman amd-ucode]"; read line
+#   echo "Press enter [pacman amd-ucode]"; read line
   while ! pacman -S --noconfirm --needed amd-ucode; do
     reconnect
   done
 fi
-echo "Press enter [cp grub /etc/default/grub]"; read line
+# echo "Press enter [cp grub /etc/default/grub]"; read line
 cp grub /etc/default/grub
-echo "Press enter [grub-mkconfig]"; read line
+# echo "Press enter [grub-mkconfig]"; read line
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #			skripte
 
-echo "Press enter [cd /tmp]"; read line
+# echo "Press enter [cd /tmp]"; read line
 cd /tmp
-echo "Press enter [mkdir git_scripts]"; read line
+# echo "Press enter [mkdir git_scripts]"; read line
 sudo -u "$username" mkdir git_scripts
-echo "Press enter [cd git_scripts]"; read line
+# echo "Press enter [cd git_scripts]"; read line
 cd git_scripts
-echo "Press enter [git archgd]"; read line
+# echo "Press enter [git archgd]"; read line
 while ! sudo -u "$username" git clone https://github.com/donaastor/archgd.git; do
   reconnect
 done
-echo "Press enter [cd archgd]"; read line
+# echo "Press enter [cd archgd]"; read line
 cd archgd
-echo "Press enter [rm .git]"; read line
+# echo "Press enter [rm .git]"; read line
 rm -rf .git
-echo "Press enter [mv scripts /home/$username/scripts]"; read line
+# echo "Press enter [mv scripts /home/$username/scripts]"; read line
 sudo -u "$username" mv scripts "/home/$username/scripts"
-echo "Press enter [mkdir /home/$username/Pictures]"; read line
+# echo "Press enter [mkdir /home/$username/Pictures]"; read line
 sudo -u "$username" mkdir "/home/$username/Pictures"
-echo "Press enter [mv poz.jpg]"; read line
+# echo "Press enter [mv poz.jpg]"; read line
 sudo -u "$username" mv poz_r.jpg "/home/$username/Pictures/poz.jpg"
-echo "Press enter [mkdir .config]"; read line
+# echo "Press enter [mkdir .config]"; read line
 sudo -u "$username" mkdir "/home/$username/.config"
 if [ $MORE_PROGS = 1 ]; then
-  echo "Press enter [mv geany config]"; read line
+#   echo "Press enter [mv geany config]"; read line
   sudo -u "$username" mv "geany" "/home/$username/.config/geany"
 fi
 
 #			getty
 
-echo "Press enter [cd /etc/systemd/system]"; read line
+# echo "Press enter [cd /etc/systemd/system]"; read line
 cd /etc/systemd/system
-echo "Press enter [mkdir getty_tty_service_d]"; read line
+# echo "Press enter [mkdir getty_tty_service_d]"; read line
 mkdir "getty@tty1.service.d"
-echo "Press enter [cd getty_tty_service_d]"; read line
+# echo "Press enter [cd getty_tty_service_d]"; read line
 cd "getty@tty1.service.d"
-echo "Press enter [autologin.conf]"; read line
+# echo "Press enter [autologin.conf]"; read line
 printf "[Service]\nExecStart=\nExecStart=-/sbin/agetty -o \'-p -f -- \\\\\\\\u\' --noclear --autologin root - \$TERM\nType=simple\n" > autologin.conf
 if [ $WIFI = 1 ]; then
-  echo "Press enter [prep ins-2, wifi]"; read line
+#   echo "Press enter [prep ins-2, wifi]"; read line
   printf "\n/bin/bash \"/home/$username/scripts/ins-2.sh\" $username \"$params\" \"$ssid_dft\"" >> "/home/$username/.bashrc"
 else
-  echo "Press enter [prep ins-2, no wifi]"; read line
+#   echo "Press enter [prep ins-2, no wifi]"; read line
   printf "\n/bin/bash \"/home/$username/scripts/ins-2.sh\" $username \"$params\"" >> "/home/$username/.bashrc"
 fi
 
 #			exit
 
-echo "Press enter [exit chroot]"; read line
+# echo "Press enter [exit chroot]"; read line
 exit 0
