baa21263d03fc415d82730f3011857a2bd288fb8
donaastor <45340801+donaastor@users.noreply.github.com>
2022-04-04 03:29:42 +0200

    Update ins-late.sh

--- scripts/ins-late.sh
+++ scripts/ins-late.sh
@@ -256,9 +256,6 @@ if [ $HIDPI = 1 ]; then
   printf "Xft.dpi: 192\n" > .Xresources
   chown $username:wheel .Xresources
 fi
-# echo "Press enter [nitrogen setup]"; read line
-# nitrogen --set-zoom-fill "/home/$username/Pictures/poz.jpg"
-# echo "Press enter [printf .xinitrc-tobe]"; read line
 if [ $AMD_GPU = 1 ]; then
   if [ $HIDPI = 1 ]; then
     printf '#!'"/bin/sh\n\n[[ -f ~/.Xresources ]] && xrdb -merge -I\$HOME ~/.Xresources\nxset s noblank\nxset s noexpose\nxset s 0 0\nxset +dpms\nxset dpms 0 180 0\nnumlockx &\nxset r rate 250 30\nxbindkeys &\n/opt/kbswtb &\nif ! pgrep -f xidlehook; then\n  xidlehook --timer 600 'systemctl suspend -i' '' &\nfi\npicom --experimental-backends &\nexport QT_SCREEN_SCALE_FACTORS=1.5\ncorectrl &\nnitrogen --restore &\nexec i3\n" > .xinitrc-tobe
@@ -274,24 +271,18 @@ else
 fi
 # echo "Press enter [printf .xinitrc]"; read line
 if [ $MORE_PROGS = 1 ]; then
-  printf '#!'"/bin/sh\n\ncd /home/$username\nsystemctl --user start pipewire-pulse\npactl set-sink-volume @DEFAULT_SINK@ 100%%\nnitrogen --set-zoom-fill Pictures/poz.jpg\nnitrogen --save\npsd\nsed -i \'s/^.*\\\\\"USE_BACKUPS\\\\\"=\\\\\"yes\\\\\".*\$/\\\\\"USE_BACKUPS\\\\\"=\\\\\"no\\\\\"/\' .config/psd/psd.conf\nsystemctl --user enable psd\nsystemctl --user start psd\nexec bash --norc -c \"cd /home/$username; mv .xinitrc-tobe .xinitrc && source .xinitrc\"\n" > .xinitrc
+  printf '#!'"/bin/sh\n\ncd /home/$username\nsystemctl --user start pipewire-pulse\npactl set-sink-volume @DEFAULT_SINK@ 100%%\npsd\nsed -i \'s/^.*\\\\\"USE_BACKUPS\\\\\"=\\\\\"yes\\\\\".*\$/\\\\\"USE_BACKUPS\\\\\"=\\\\\"no\\\\\"/\' .config/psd/psd.conf\nsystemctl --user enable psd\nsystemctl --user start psd\nexec bash --norc -c \"cd /home/$username; mv .xinitrc-tobe .xinitrc && source .xinitrc\"\n" > .xinitrc
 else
-  printf '#!'"/bin/sh\n\ncd /home/$username\nsystemctl --user start pipewire-pulse\npactl set-sink-volume @DEFAULT_SINK@ 100%%\nnitrogen --set-zoom-fill Pictures/poz.jpg\nnitrogen --save\nexec bash --norc -c \"cd /home/$username; mv .xinitrc-tobe .xinitrc && source .xinitrc\"\n" > .xinitrc
+  printf '#!'"/bin/sh\n\ncd /home/$username\nsystemctl --user start pipewire-pulse\npactl set-sink-volume @DEFAULT_SINK@ 100%%\nexec bash --norc -c \"cd /home/$username; mv .xinitrc-tobe .xinitrc && source .xinitrc\"\n" > .xinitrc
 fi
-chown $username:wheel .xinitrc .xinitrc-tobe
-# echo "Press enter [cp .bashrc]"; read line
+printf "[xin_-1]\nfile=/home/$username/Pictures/poz.jpg\nmode=5\nbgcolor=#000000\n" > .config/nitrogen/bg-saved.cfg
+chown $username:wheel .xinitrc .xinitrc-tobe .config/nitrogen/bg-saved.cfg
 cp "/home/$username/.bashrc" /tmp/bashrc_radni
-# echo "Press enter [chmod bashrc_radni]"; read line
 chmod 777 /tmp/bashrc_radni
-# echo "Press enter [sed remove 'alias ls']"; read line
 sed -i 's/^alias ls.*$//' /tmp/bashrc_radni
-# echo "Press enter [printf startx > bashrc_radni]"; read line
 printf "if [ -z \"\${DISPLAY}\" ] && [ \"\${XDG_VTNR}\" -eq 1 ]; then\n  startx\nfi\n" >> /tmp/bashrc_radni
-# echo "Press enter [cp .bashrc]"; read line
 sudo -u "$username" cp /tmp/bashrc_radni "/home/$username/.bashrc"
-# echo "Press enter [printf numlock]"; read line
 printf '#!/bin/bash\n\nfor tty in /dev/tty{1..6}\ndo\n  /usr/bin/setleds -D +num < \"$tty\";\ndone\n' > /usr/local/bin/numlock
-# echo "Press enter [chmod numlock]"; read line
 chmod 755 /usr/local/bin/numlock
 # echo "Press enter [printf numlock.service]"; read line
 printf "[Unit]\nDescription=numlock\n\n[Service]\nExecStart=/usr/local/bin/numlock\nStandardInput=tty\nRemainAfterExit=yes\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/numlock.service
