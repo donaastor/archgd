2f38448a5cfbb167cffa4bb4690e40671cf6b341
donaastor <45340801+donaastor@users.noreply.github.com>
2022-04-05 02:43:11 +0200

    Update ins-late.sh

--- scripts/ins-late.sh
+++ scripts/ins-late.sh
@@ -235,11 +235,6 @@ else
     printf '#!'"/bin/sh\n\n[[ -f ~/.Xresources ]] && xrdb -merge -I\$HOME ~/.Xresources\nxset s noblank\nxset s noexpose\nxset s 0 0\nxset +dpms\nxset dpms 0 180 0\nnumlockx &\nxset r rate 250 30\nxbindkeys &\n/opt/kbswtb &\nif ! pgrep -f xidlehook; then\n  xidlehook --timer 600 'systemctl suspend -i' '' &\nfi\npicom --experimental-backends &\nnitrogen --restore &\nexec i3\n" > .xinitrc-tobe
   fi
 fi
-# if [ $MORE_PROGS = 1 ]; then
-#   printf '#!'"/bin/sh\n\ncd /home/$username\nsystemctl --user start pipewire-pulse\npactl set-sink-volume @DEFAULT_SINK@ 100%%\npsd\nsed -i \'s/^.*USE_BACKUPS=\\\\\"yes\\\\\".*\$/USE_BACKUPS=\\\\\"no\\\\\"/\' .config/psd/psd.conf\nsystemctl --user enable psd\nsystemctl --user start psd\nexec bash --norc -c \"cd /home/$username; mv .xinitrc-tobe .xinitrc && source .xinitrc\"\n" > .xinitrc
-# else
-#   printf '#!'"/bin/sh\n\ncd /home/$username\nsystemctl --user start pipewire-pulse\npactl set-sink-volume @DEFAULT_SINK@ 100%%\nexec bash --norc -c \"cd /home/$username; mv .xinitrc-tobe .xinitrc && source .xinitrc\"\n" > .xinitrc
-# fi
 if [ $MORE_PROGS = 1 ]; then
   xit_ad="psd\nsed -i 's/^.*USE_BACKUPS=\\\\\"yes\\\\\".*$/USE_BACKUPS=\\\\\"no\\\\\"/' .config/psd/psd.conf\nsystemctl --user enable psd\nsystemctl --user start psd\n"
 else
