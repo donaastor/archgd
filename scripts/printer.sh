0e9460fc8c4cfe2a1357132b19cc517f32c284a2
donaastor <45340801+donaastor@users.noreply.github.com>
2022-05-05 17:00:27 +0200

    Update printer.sh

--- scripts/printer.sh
+++ scripts/printer.sh
@@ -2,22 +2,39 @@
 
 sudo pacman -S cups hplip
 pikaur -S hplip-plugin
-sudo sed 's/^#\(net.ipv4.ip_forward\).*$/\1=1/' -i /etc/ufw/sysctl.conf
-sudo sed 's/^\(DEFAULT_OUTPUT_POLICY\).*$/\1="ACCEPT"/' -i /etc/default/ufw
-printf "Real printer IP: "
-read real_ip
-printf "Fake new printer IP: "
-read fake_ip
-printf "Ports that are forwarded to 80,9100,161,162 (separate by spaces):\n"
-read fw_ports
-fwpa=( $fw_ports )
-pt80=${fwpa[0]}
-pt9100=${fwpa[1]}
-pt161=${fwpa[2]}
-pt162=${fwpa[3]}
-sudo sed "s/^\(\*filter\)$/\*nat\n:OUTPUT ACCEPT \[0:0\]\n-A OUTPUT -p tcp -d $fake_ip --dport 80   -j DNAT --to-destination $real_ip:$pt80\n-A OUTPUT -p tcp -d $fake_ip --dport 9100 -j DNAT --to-destination $real_ip:$pt9100\n-A OUTPUT -p udp -d $fake_ip --dport 9100 -j DNAT --to-destination $real_ip:$pt9100\n-A OUTPUT -p tcp -d $fake_ip --dport 161  -j DNAT --to-destination $real_ip:$pt161\n-A OUTPUT -p udp -d $fake_ip --dport 161  -j DNAT --to-destination $real_ip:$pt161\n-A OUTPUT -p tcp -d $fake_ip --dport 162  -j DNAT --to-destination $real_ip:$pt162\n-A OUTPUT -p udp -d $fake_ip --dport 162  -j DNAT --to-destination $real_ip:$pt162\nCOMMIT\n\1/" -i /etc/ufw/before.rules
-sudo ufw disable
-sudo ufw enable
+Q1_F=1
+while [ $Q1_F == 1 ]; do
+  printf "Are you accessing your printer directly (yes/no)? "
+  read dir_odg
+  if [ "$dir_odg" == "yes" ]; then
+    FWD=0
+    Q1_F=0
+  elif [ "$dir_odg" == "no" ]; then
+    FWD=1
+    Q1_F=0
+  fi
+done
+if [ $FWD == 1 ]; then
+  sudo sed 's/^#\(net.ipv4.ip_forward\).*$/\1=1/' -i /etc/ufw/sysctl.conf
+  sudo sed 's/^\(DEFAULT_OUTPUT_POLICY\).*$/\1="ACCEPT"/' -i /etc/default/ufw
+  printf "Real printer IP: "
+  read real_ip
+  printf "Fake new printer IP: "
+  read fake_ip
+  printf "Ports that are forwarded to 80,9100,161,162 (separate by spaces):\n"
+  read fw_ports
+  fwpa=( $fw_ports )
+  pt80=${fwpa[0]}
+  pt9100=${fwpa[1]}
+  pt161=${fwpa[2]}
+  pt162=${fwpa[3]}
+  sudo sed "s/^\(\*filter\)$/\*nat\n:OUTPUT ACCEPT \[0:0\]\n-A OUTPUT -p tcp -d $fake_ip --dport 80   -j DNAT --to-destination $real_ip:$pt80\n-A OUTPUT -p tcp -d $fake_ip --dport 9100 -j DNAT --to-destination $real_ip:$pt9100\n-A OUTPUT -p udp -d $fake_ip --dport 9100 -j DNAT --to-destination $real_ip:$pt9100\n-A OUTPUT -p tcp -d $fake_ip --dport 161  -j DNAT --to-destination $real_ip:$pt161\n-A OUTPUT -p udp -d $fake_ip --dport 161  -j DNAT --to-destination $real_ip:$pt161\n-A OUTPUT -p tcp -d $fake_ip --dport 162  -j DNAT --to-destination $real_ip:$pt162\n-A OUTPUT -p udp -d $fake_ip --dport 162  -j DNAT --to-destination $real_ip:$pt162\nCOMMIT\n\1/" -i /etc/ufw/before.rules
+  sudo ufw disable
+  sudo ufw enable
+else
+  printf "Printer IP: "
+  read fake_ip
+fi
 sudo systemctl start cups
 hp-setup -i -a -x $fake_ip
 p_name=$( lpstat -d )
