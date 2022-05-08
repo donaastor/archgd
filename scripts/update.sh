4a9e4d609e1a7763e48e66797e506e3d5fe7be2d
donaastor <45340801+donaastor@users.noreply.github.com>
2022-05-08 04:01:54 +0200

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,14 +7,13 @@ if [ "$username" = "root" ]; then
   printf "You are running this script as root!\n\n"
 fi
 
-rn="$RANDOM""$RANDOM""$RANDOM"
 cd /tmp
 if ! [ -d current_scripts_from_git ]; then
   mkdir current_scripts_from_git
 fi
 cd current_scripts_from_git
 for scn in $scripts; do
-  curl --no-sessionid "https://raw.githubusercontent.com/donaastor/archgd/main/scripts/$scn.sh?haha=$rn" > "$scn.sh"
+  curl --no-sessionid "https://raw.githubusercontent.com/donaastor/archgd/main/scripts/$scn.sh" > "$scn.sh"
 done
 
 hash2=""
