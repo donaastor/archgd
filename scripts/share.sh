cd0b8967a1509b1dce8575d06ada10954b2406e3
donaastor <45340801+donaastor@users.noreply.github.com>
2022-05-15 14:48:27 +0200

    Update share.sh

--- scripts/share.sh
+++ scripts/share.sh
@@ -101,6 +101,8 @@ sudo mount -t tmpfs tmpfs $n_HOME/sharing/write -o defaults,size=2048M
 n_USER=$USER
 printf "Name your share: "
 read loc_name
+printf "Give bash nickname to your share: "
+read loc_nick
 if [ -f /etc/samba/smb.conf ]; then
   sudo mv /etc/samba/smb.conf /etc/samba/smb-before-share-script.conf
 fi
@@ -120,4 +122,6 @@ printf "Username: "
 read win_user
 printf "Password for connecting to \"$rem_name\" as $win_user: "
 read win_pass
-sed "s/^\\(PS1=.\\[\\\\u@\\\\h \\\\W\\]\\\\. .\\)$/\\1\nalias shares='sudo systemctl restart smb nmb; sudo mount -t cifs \\/\\/$win_ip\\/$rem_name \\/home\\/$USER\\/sharing\\/read -o port=$win_port,workgroup=WORKGROUP,iocharset=utf8,username=$win_user,password=$win_pass,cache=none'\nalias shoff='sudo systemctl stop smb nmb; sudo umount \\/home\\/$USER\\/sharing\\/read'\n/" -i $HOME/.bashrc
+printf "Bash nickname for remote share: "
+read win_nick
+sed "s/^\\(PS1=.\\[\\\\u@\\\\h \\\\W\\]\\\\. .\\)$/\\1\nalias shon_$loc_nick='sudo systemctl restart smb nmb'\nalias shon_$win_nick='sudo mount -t cifs \\/\\/$win_ip\\/$rem_name \\/home\\/$USER\\/sharing\\/read -o port=$win_port,workgroup=WORKGROUP,iocharset=utf8,username=$win_user,password=$win_pass,cache=none'\nshon() {\n  if \\[ -z \"\$1\" \\]; then\n    shon_$loc_nick\n    shon_$win_nick\n  else\n    if \\[ \"\$1\" = \"$loc_nick\" \\]; then\n      shon_$loc_nick\n    elif \\[ \"\$1\" = \"$win_nick\" \\]; then\n      shon_$win_nick\n    fi\n  fi\n}\nalias shoff_$loc_nick='sudo systemctl stop smb nmb'\nalias shoff_$win_nick='sudo umount \\/home\\/$USER\\/sharing\\/read'\nshoff() {\n  if \\[ -z \"\$1\" \\]; then\n    shoff_$loc_nick\n    shoff_$win_nick\n  else\n    if \\[ \"\$1\" = \"$loc_nick\" \\]; then\n      shoff_$loc_nick\n    elif \\[ \"\$1\" = \"$win_nick\" \\]; then\n      shoff_$win_nick\n    fi\n  fi\n}\n/" -i $HOME/.bashrc
