7ce517b2c4064926d616ecd9511e053f243a7426
donaastor <45340801+donaastor@users.noreply.github.com>
2022-05-17 07:20:24 +0200

    Update filetypes.cpp

--- geany/filedefs/filetypes.cpp
+++ geany/filedefs/filetypes.cpp
@@ -1,9 +1,9 @@
 [build-menu]
 FT_00_LB=_Compile
-FT_00_CM=g++ "%f" -o "%e" -pipe -fwrapv -fno-plt -fno-semantic-interposition -std=c++20 -mcmodel=large -march=x86-64 -mtune=generic -Wshadow -Wno-unused-result -Wall -O3 -L /usr/lib -Wl,--as-needed -lm -lz -lpthread -lutil -ldl -lrt -lcrypt
+FT_00_CM=g++ "%f" -o "%e" -pipe -fwrapv -fno-plt -fno-semantic-interposition -std=c++20 -mcmodel=large -march=x86-64 -mtune=generic -Wshadow -Wno-unused-result -Wall -O3 -L /usr/lib -Wl,--as-needed -lm -lz -lpthread -lutil -ldl -lrt -lgcrypt
 FT_00_WD=
 FT_01_LB=_Build
-FT_01_CM=g++ "%f" -o "%e" -pipe -fwrapv -fno-plt -fno-semantic-interposition -std=c++20 -mcmodel=large -march=x86-64 -mtune=generic -Wshadow -Wno-unused-result -Wall -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -L /usr/lib -Wl,--as-needed -lm -lz -lpthread -lutil -ldl -lrt -lcrypt
+FT_01_CM=g++ "%f" -o "%e" -pipe -fwrapv -fno-plt -fno-semantic-interposition -std=c++20 -mcmodel=large -march=x86-64 -mtune=generic -Wshadow -Wno-unused-result -Wall -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -L /usr/lib -Wl,--as-needed -lm -lz -lpthread -lutil -ldl -lrt -lgcrypt
 FT_01_WD=
 FT_02_LB=_Lint
 FT_02_CM=cppcheck --language=c++ --enable=warning,style --template=gcc "%f"
