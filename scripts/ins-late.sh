53dc34f75874a993ba66b8e2722d4ad9881e7a77
donaastor <45340801+donaastor@users.noreply.github.com>
2022-06-21 02:43:42 +0200

    Update ins-late.sh

--- scripts/ins-late.sh
+++ scripts/ins-late.sh
@@ -172,7 +172,7 @@ sed -i 's/noedit = no/noedit = yes/' "/tmp/pikaur_radni.conf"
 sed -i 's/donteditbydefault = no/donteditbydefault = yes/' "/tmp/pikaur_radni.conf"
 sudo -u "$username" cp "/tmp/pikaur_radni.conf" "/home/$username/.config/pikaur.conf"
 if [ $MORE_PROGS = 1 ]; then
-  ad_progs="texlive-formatsextra texlive-langcyrillic texlive-latexextra texlive-science openssh tmux vlc feh zathura zathura-djvu zathura-pdf-poppler flameshot calc geany geany-plugins pcmanfm-gtk3 gvfs simplescreenrecorder gimp transmission-qt crypto++"
+  ad_progs="texlive-formatsextra texlive-langcyrillic texlive-latexextra texlive-science openssh tmux vlc feh zathura zathura-djvu zathura-pdf-poppler flameshot calc geany geany-plugins pcmanfm-gtk3 gvfs simplescreenrecorder gimp transmission-qt torsocks php"
   aur_progs=""
 else
   ad_progs=""
