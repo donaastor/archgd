5f96162a0a5a7ca706ae54e5866dd331c6efbdf5
donaastor <45340801+donaastor@users.noreply.github.com>
2022-10-24 00:08:15 +0200

    Update ins.sh

--- scripts/ins.sh
+++ scripts/ins.sh
@@ -6,11 +6,11 @@ efiquit() {
 }
 
 num_of_args=$#
-if [ "$num_of_args" -lt "5" ]; then
+if [ $num_of_args -lt 5 ]; then
   printf "At least 5 arguments expected:\n1. BIOS type: BIOS or EFI\n2. Boot partition\n3. Root partition\n4. (optional, only if BIOS selected) name of the whole drive\n5. username\n6. parameters... a string of 0's and 1's (and other digits)\n    first bit: set:\n      0 - if you have an Intel CPU,\n      1 - if you have an AMD pre-Zen CPU, or\n      2 - if you have an AMD Zen CPU\n    second bit: set:\n      0 - for no graphics driver,\n      1 - if you have an AMD GCN-2 or older GPU,\n      2 - if you have a GCN-1 or GCN-2, but want newer drivers,\n      3 - if you have a newer AMD GPU (for newer drivers), or\n      4 - if you have an RDNA 2 or newer GPU\n    third bit: set if WiFi available and ethernet not available\n    fourth bit: set if you want to set up for HiDPI\n    fifth bit: set if you have a battery\n    sixth bit: set if you want more programs installed\n7. (optional, only if WiFi selected) SSID\n"
   exit 1
 fi
-if [ "$1" = "EFI" ]; then
+if [ "$1" = EFI ]; then
   efipop=""
   if ! efipop="$(ls /sys/firmware/efi/efivars)"; then
     efiquit
@@ -22,7 +22,7 @@ if [ "$1" = "EFI" ]; then
   EFI=1
   username="$4"
   params="$5"
-elif [ "$1" = "BIOS" ]; then
+elif [ "$1" = BIOS ]; then
   EFI=0
   if [ -z "$6" ]; then
     echo "Missing arguments"
@@ -41,7 +41,7 @@ CPU="${params:0:1}"
 if ! [[ $CPU =~ [0-2] ]]; then CPU=0; fi
 GPU="${params:1:1}"
 if ! [[ $GPU =~ [0-4] ]]; then GPU=0; fi
-if [ "${params:2:1}" = "1" ]; then
+if [ "${params:2:1}" = 1 ]; then
   WIFI=1
   if [ $EFI = 1 ]; then
     if [ -z "$6" ]; then
@@ -59,17 +59,17 @@ if [ "${params:2:1}" = "1" ]; then
 else
   WIFI=0
 fi
-if [ "${params:3:1}" = "1" ]; then
+if [ "${params:3:1}" = 1 ]; then
   HIDPI=1
 else
   HIDPI=0
 fi
-if [ "${params:4:1}" = "1" ]; then
+if [ "${params:4:1}" = 1 ]; then
   BATT=1
 else
   BATT=0
 fi
-if [ "${params:5:1}" = "1" ]; then
+if [ "${params:5:1}" = 1 ]; then
   MORE_PROGS=1
 else
   MORE_PROGS=0
@@ -99,7 +99,7 @@ reconnect() {
       if getent hosts archlinux.org; then
         JOS=0
       else
-        printf "."
+        printf .
       fi
     fi
     WWAIT=1
@@ -112,7 +112,7 @@ if [ $WIFI = 1 ]; then
   while ! curl https://raw.githubusercontent.com/donaastor/archgd/main/scripts/wifi-guard.sh > /tmp/wifi-guard.sh; do
     reconnect
   done
-  2>/dev/null 1>/dev/null bash "/tmp/wifi-guard.sh" "$ssid_dft" &
+  2>/dev/null 1>/dev/null bash /tmp/wifi-guard.sh "$ssid_dft" &
 fi
 
 timedatectl set-ntp true
@@ -141,9 +141,9 @@ done
 if [ $WIFI = 1 ]; then
   cp -r /var/lib/iwd /mnt/var/lib/iwd
 fi
-mkdir "/mnt/root/tren"
-mount -t tmpfs tmpfs -o defaults,size=128M "/mnt/root/tren"
-genfstab -U /mnt >> "/mnt/root/tren/fstab_radni"
+mkdir /mnt/root/tren
+mount -t tmpfs tmpfs -o defaults,size=128M /mnt/root/tren
+genfstab -U /mnt >> /mnt/root/tren/fstab_radni
 cd /
 while ! curl https://raw.githubusercontent.com/donaastor/archgd/main/scripts/ins-chroot.sh > /mnt/root/tren/ins-chroot.sh; do
   reconnect
