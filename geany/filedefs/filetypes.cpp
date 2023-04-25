4e94f28e8cdec232751f24948f06a17d6c534104
donaastor <45340801+donaastor@users.noreply.github.com>
2023-04-26 00:04:15 +0200

    Update filetypes.cpp

--- geany/filedefs/filetypes.cpp
+++ geany/filedefs/filetypes.cpp
@@ -1,7 +1,7 @@
 [build-menu]
 FT_00_LB=_Compile
-FT_00_CM=g++ "%f" -o "%e" -pipe -fwrapv -fno-plt -fno-semantic-interposition -std=gnu++23 -O3 -mcmodel=large -maddress-mode=long -m64 -march=znver3 -mtune=znver3 -mabm -mbmi -mbmi2 -mmovbe -Wshadow -Wno-unused-result -Wall -I/usr/local/include/mylib -I/usr/include/python3.10 -L/usr/lib -L/usr/local/include/mylib/shared -Wl,--as-needed -lpython3.10 -lm -lz -lpthread -lutil -ldl -lrt -lcrypt -lgcrypt -lcurl -lncurses -Wl,-Bstatic -ltomcrypt -ltfm -Wl,-Bdynamic
+FT_00_CM=cppc "%f"
 FT_00_WD=
 FT_01_LB=_Build
-FT_01_CM=g++ "%f" -o "%e" -pipe -fwrapv -fno-plt -fno-semantic-interposition -std=gnu++23 -mcmodel=large -maddress-mode=long -m64 -march=znver3 -mtune=znver3 -mabm -mbmi -mbmi2 -mmovbe -Wshadow -Wno-unused-result -Wall -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -I/usr/local/include/mylib -I/usr/include/python3.10 -L/usr/lib -L/usr/local/include/mylib/shared -Wl,--as-needed -lpython3.10 -lm -lz -lpthread -lutil -ldl -lrt -lcrypt -lgcrypt -lcurl -lncurses -Wl,-Bstatic -ltomcrypt -ltfm -Wl,-Bdynamic
+FT_01_CM=cppc -d "%d"
 FT_01_WD=
